7fde3d575d021a61c1168493773face554f7fab0
EmzedLegend <payzullayevdilshodbek13@gmail.com>
2026-08-30 00:46:22 +0500

    feat: add global error handler

---
+++ middleware/errorHandler.js
@@ -0,0 +1,1 @@
+module.exports=function errorHandler(err,req,res,next){if(res.headersSent)return next(err);const status=Number(err.status||500);res.status(status).json({success:false,message:status>=500?'Internal server error':err.message,error:err.code||'INTERNAL_ERROR'})};
